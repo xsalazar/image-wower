635f01638d1222bf1299935e454950030b02ac2a
Xavier Salazar <x.salazar1729@gmail.com>
2022-11-03 15:09:05 -0700

    Reimplement background removal and add random background gif selection

--- app/index.js
+++ app/index.js
@@ -10,46 +10,68 @@ exports.handler = async (event, context) => {
   }
 
   try {
-    // console.log("Sending request to remove.bg");
+    console.log("Sending request to remove.bg");
 
     // Send request for background removal
-    // const removebgRes = await axios.post(
-    //   "https://api.remove.bg/v1.0/removebg",
-    //   {
-    //     image_file_b64: event.body,
-    //     size: "preview",
-    //     type: "person",
-    //   },
-    //   {
-    //     responseType: "arraybuffer",
-    //     headers: {
-    //       "X-Api-Key": `${process.env.REMOVE_BG_API_KEY}`,
-    //     },
-    //   }
-    // );
+    const removebgRes = await axios.post(
+      "https://api.remove.bg/v1.0/removebg",
+      {
+        image_file_b64: event.body,
+        size: "preview",
+        type: "person",
+      },
+      {
+        responseType: "arraybuffer",
+        headers: {
+          "X-Api-Key": `${process.env.REMOVE_BG_API_KEY}`,
+        },
+      }
+    );
 
-    // Resize to 500x500 and save to tmp
+    // Resize result to 500x500 and save to tmp directory
     const removedBgPath = `/tmp/${uuidv4()}.png`;
-    await sharp(Buffer.from(event.body, "base64"))
+    await sharp(removebgRes.data)
       .resize({ width: 500, height: 500 })
       .toFile(removedBgPath);
 
     console.log("Sending request to Giphy");
 
     // Get GIF by ID from Giphy API
-    const gifId = "Ck80ojSw2VQWfwFfnY";
+    const gifs = [
+      "Ck80ojSw2VQWfwFfnY",
+      "cS8Dk5NywlGLvaIV0r",
+      "iGkCVENBbn6230aiPp",
+      "sBRGYMFHTdGqorjlyO",
+      "tLVuQirOR5NB4qix5P",
+      "T8QbiYtxoglu6BpA3s",
+      "aaupIDpb0zN3ME96ll",
+      "g9ScOoUhWABOOu2pZ3",
+      "YvwleWJv6NRLDC52K6",
+      "3rDiaWkdR76tdKXIDd",
+      "zw9QEsVqdCgHehelNO",
+      "CokjjWNnFJ3fZ0YN02",
+      "PGFzJs26BscwZybBLI",
+      "dtVbhQZHcsYbbCoe1y",
+      "F7010euR3WZBhwYsbM",
+      "kfs62t4onP5xx8hPYi",
+      "P3Eq0szC9nBXdFPsmq",
+      "dBMW2ykthSv7ix64Ou",
+      "26vUyWzmjBIlNJvRC",
+      "h7dhRBp5YXZqQ0mr3f",
+    ];
+    const gifId = gifs[Math.floor(Math.random() * gifs.length)];
     const giphyRes = await axios.get(
       `https://api.giphy.com/v1/gifs/${gifId}?api_key=${process.env.GIPHY_API_KEY}`
     );
 
-    console.log("Downloading raw gif");
+    console.log("Downloading gif");
 
     // Download GIF
     const rawGif = await axios.get(giphyRes.data.data.images.original.url, {
       responseType: "arraybuffer",
     });
 
-    // Resize to 500x500 and save file
+    // Resize to 500x500 and save file to tmp directory
     const gifPath = `/tmp/${uuidv4()}.gif`;
     await sharp(rawGif.data, {
       animated: true,
@@ -57,9 +79,9 @@ exports.handler = async (event, context) => {
       .resize({ width: 500, height: 500 })
       .toFile(gifPath);
 
-    console.log("Combining");
+    console.log("Wowifying");
 
-    // Combine gif with result
+    // Combine gif with result and save file to tmp directory
     const compositePath = `/tmp/${uuidv4()}.gif`;
     await sharp(gifPath, {
       animated: true,
@@ -74,7 +96,10 @@ exports.handler = async (event, context) => {
       ])
       .toFile(compositePath);
 
-    const fullSize = await sharp(compositePath, { animated: true }).toBuffer();
+    // Generate original and small size data
+    const originalSize = await sharp(compositePath, {
+      animated: true,
+    }).toBuffer();
     const smallSize = await sharp(compositePath, { animated: true })
       .resize({ width: 64, height: 64 })
       .toBuffer();
@@ -87,7 +112,7 @@ exports.handler = async (event, context) => {
       statusCode: 200,
       headers: { "content-type": "application/json" },
       body: JSON.stringify({
-        wowifiedFull: fullSize.toString("base64"),
+        wowifiedOriginal: originalSize.toString("base64"),
         wowifiedSmall: smallSize.toString("base64"),
       }),
     };
