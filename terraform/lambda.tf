3a9852fcf476f674cef9c8e70e200520cb227d95
Xavier Salazar <x.salazar1729@gmail.com>
2022-11-18 22:32:14 -0800

    Update Lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,44 +1,21 @@
-variable "remove_bg_api_key" {
-  type = string
-}
-
-variable "giphy_api_key" {
-  type = string
-}
-
 resource "aws_lambda_function" "instance" {
   function_name = "image-wower"
-  filename      = "${path.module}/dummy-lambda-package/lambda.zip" // Simple hello world application
+  image_uri     = "368081326042.dkr.ecr.us-west-2.amazonaws.com/image-wower-ecr-repo:aeb86a74f0779287dc23947007cadaebfa4454a0"
   role          = aws_iam_role.instance.arn
-  handler       = "index.handler"
-  runtime       = "nodejs16.x"
-  timeout       = 60   // seconds
+  timeout       = 30   // seconds -- matches API Gateway integration timeout limit
   memory_size   = 4096 // MB
 
   ephemeral_storage {
     size = 4096 # Min 512 MB and the Max 10240 MB
   }
 
-  environment {
-    variables = {
-      REMOVE_BG_API_KEY = var.remove_bg_api_key,
-      GIPHY_API_KEY     = var.giphy_api_key
-      U2NET_HOME        = "/tmp/u2net/"
-      GIF_BUCKET        = aws_s3_bucket.instance.id
-      REMBG_BUCKET      = aws_s3_bucket.rembg.id
-    }
-  }
-
-  layers = [
-    aws_lambda_layer_version.instance.arn
-  ]
-
   // Since CI/CD will deploy this application externally, these do not need to be tracked after creation
   lifecycle {
     ignore_changes = [
       last_modified,
       source_code_hash,
-      source_code_size
+      source_code_size,
+      image_uri
     ]
   }
 }
@@ -46,12 +23,6 @@ resource "aws_lambda_function" "instance" {
 resource "aws_cloudwatch_log_group" "instance" {
   name              = "/aws/lambda/${aws_lambda_function.instance.function_name}"
   retention_in_days = 30 // days
-}
-
-resource "aws_lambda_layer_version" "instance" {
-  filename            = "${path.module}/imagemagick-lambda-layer-package/imagemagick.zip"
-  layer_name          = "image-wower-imagemagick-layer"
-  compatible_runtimes = ["nodejs16.x"]
 }
 
 resource "aws_s3_bucket" "instance" {
