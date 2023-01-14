469d8201880b6a72ffd27f4746ec95d89ef812c9
Xavier Salazar <x.salazar1729@gmail.com>
2023-01-13 19:53:39 -0800

    Try to update application to support async request processing and polling 🤞

--- app/app.js
+++ app/app.js
@@ -6,147 +6,220 @@ const sharp = require("sharp");
 exports.handler = async (event, context) => {
   console.log(JSON.stringify(event));
 
-  if (!event.body) {
-    return;
+  // Check S3 for result, return it, if found
+  if (event.queryStringParameters && event.queryStringParameters.wowToken) {
+    const token = event.queryStringParameters.wowToken;
+    const s3 = new AWS.S3();
+
+    try {
+      const data = await s3
+        .getObject({ Bucket: "image-wower-data", Key: token })
+        .promise();
+
+      // Delete object after retrieval
+      await s3
+        .deleteObject({ Bucket: "image-wower-data", Key: token })
+        .promise();
+
+      return {
+        cookies: [],
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: { "content-type": "application/json" },
+        body: data.Body,
+      };
+    } catch (e) {
+      return {
+        cookies: [],
+        isBase64Encoded: false,
+        statusCode: 404,
+        headers: {},
+        body: "",
+      };
+    }
   }
 
-  try {
-    if (!fs.existsSync(`/tmp/libs/u2net`)) {
-      console.log("Copying rembg library to /tmp directory");
+  // Check if this was triggered by SQS
+  else if (event.Records && event.Records.length > 0) {
+    // Process each record, and save data to S3
+    for (let i = 0; i < event.Records.length; i++) {
+      const record = event.Records[i];
+
+      const { body, receiptHandle } = record;
+      const { token } = record.messageAttributes;
+
+      console.log(`Processing message: ${token}`);
+
+      if (!fs.existsSync(`/tmp/libs/u2net`)) {
+        console.log("Copying rembg library to /tmp directory");
+        execSync(
+          `mkdir -p /tmp/libs/u2net && cp ./libs/u2net/u2net_human_seg.onnx /tmp/libs/u2net/u2net_human_seg.onnx`
+        );
+      }
+      console.log("Removing background");
+
+      const inputPath = `/tmp/${uuidv4()}.png`;
+      const removedBgImagePath = `/tmp/${uuidv4()}.png`;
+
+      // Load file, normalize size, and save
+      await sharp(Buffer.from(body, "base64"))
+        .resize({
+          width: 500,
+          height: 500,
+        })
+        .toFile(inputPath);
+
+      // Remove background
       execSync(
-        `mkdir -p /tmp/libs/u2net && cp ./libs/u2net/u2net_human_seg.onnx /tmp/libs/u2net/u2net_human_seg.onnx`
+        `rembg i --model u2net_human_seg ${inputPath} ${removedBgImagePath}`
       );
-    }
 
-    console.log("Removing background");
+      console.log("Getting random gif");
 
-    const inputPath = `/tmp/${uuidv4()}.png`;
-    const removedBgImagePath = `/tmp/${uuidv4()}.png`;
+      // Get random GIF ID
+      const gifs = [
+        "Ck80ojSw2VQWfwFfnY",
+        "cS8Dk5NywlGLvaIV0r",
+        "iGkCVENBbn6230aiPp",
+        "sBRGYMFHTdGqorjlyO",
+        "tLVuQirOR5NB4qix5P",
+        "T8QbiYtxoglu6BpA3s",
+        "aaupIDpb0zN3ME96ll",
+        "YvwleWJv6NRLDC52K6",
+        "3rDiaWkdR76tdKXIDd",
+        "CokjjWNnFJ3fZ0YN02",
+        "dBMW2ykthSv7ix64Ou",
+        "26vUyWzmjBIlNJvRC",
+        "h7dhRBp5YXZqQ0mr3f",
+        "koyjGfQHIZQKk",
+        "kkoRgXbTCPY3K",
+        "rXNES6I8A7hEcq2uy1",
+        "SKga1r0b9l6ep2khVQ",
+        "bXR6c79iS8L4qa8UOx",
+        "PbfVmhMI9SvaKcUOsb",
+        "NjvLuSaJXKmjz0UDcf",
+        "xThuW2fbatiCsyY3zW",
+        "3o6Ztb45EYezY9x9gQ",
+        "3ohzdZO0nAL1H2LdMA",
+        "xUPGcknoZZseQLFvws",
+        "SYrMAmJZT4YcU",
+        "xULW8GTX7rLWv8iQ4E",
+        "3ohzdOFQWcCZA8dRT2",
+        "l0MYMranKNhMUFveE",
+        "l378oRMuApI3a35Cg",
+        "3o7TKVA3gcjiKcFuV2",
+        "3oEduJXGwLsIe1RRPG",
+        "3og0IGjLXRttYbbtcc",
+        "n8k3O3KWXbPrPfsm7s",
+        "evlvEhB86RQidkiVBO",
+        "3oFzmiu86mdcjOsjOU",
+        "2bXyklhc7qQv0dTVXr",
+        "26xBs1E58r3ZHYvgQ",
+      ];
 
-    // Load file, normalize size, and save
-    await sharp(Buffer.from(event.body, "base64"))
-      .resize({
-        width: 500,
-        height: 500,
+      const gifId = gifs[Math.floor(Math.random() * gifs.length)];
+      const gifPath = `./libs/gifs/${gifId}.webp`;
+
+      console.log("Wowifying");
+
+      // Combine gif with result and save file to tmp directory
+      const wowifiedOriginalSizePath = `/tmp/${uuidv4()}.webp`;
+
+      await sharp(gifPath, {
+        animated: true,
       })
-      .toFile(inputPath);
+        .composite([
+          {
+            input: removedBgImagePath,
+            tile: true,
+            top: 0,
+            left: 0,
+          },
+        ])
+        .webp()
+        .toFile(wowifiedOriginalSizePath);
 
-    // Remove background
-    execSync(
-      `rembg i --model u2net_human_seg ${inputPath} ${removedBgImagePath}`
-    );
+      const wowifiedSmallSizePath = `/tmp/${uuidv4()}.webp`;
+      await sharp(wowifiedOriginalSizePath, { animated: true })
+        .resize({ width: 64, height: 64 })
+        .webp({ lossless: true })
+        .toFile(wowifiedSmallSizePath);
 
-    console.log("Getting random gif");
+      // Generate original and small size data
+      const originalSize = (
+        await sharp(wowifiedOriginalSizePath, {
+          animated: true,
+        }).toBuffer()
+      ).toString("base64");
 
-    // Get random GIF ID
-    const gifs = [
-      "Ck80ojSw2VQWfwFfnY",
-      "cS8Dk5NywlGLvaIV0r",
-      "iGkCVENBbn6230aiPp",
-      "sBRGYMFHTdGqorjlyO",
-      "tLVuQirOR5NB4qix5P",
-      "T8QbiYtxoglu6BpA3s",
-      "aaupIDpb0zN3ME96ll",
-      "YvwleWJv6NRLDC52K6",
-      "3rDiaWkdR76tdKXIDd",
-      "CokjjWNnFJ3fZ0YN02",
-      "dBMW2ykthSv7ix64Ou",
-      "26vUyWzmjBIlNJvRC",
-      "h7dhRBp5YXZqQ0mr3f",
-      "koyjGfQHIZQKk",
-      "kkoRgXbTCPY3K",
-      "rXNES6I8A7hEcq2uy1",
-      "SKga1r0b9l6ep2khVQ",
-      "bXR6c79iS8L4qa8UOx",
-      "PbfVmhMI9SvaKcUOsb",
-      "NjvLuSaJXKmjz0UDcf",
-      "xThuW2fbatiCsyY3zW",
-      "3o6Ztb45EYezY9x9gQ",
-      "3ohzdZO0nAL1H2LdMA",
-      "xUPGcknoZZseQLFvws",
-      "SYrMAmJZT4YcU",
-      "xULW8GTX7rLWv8iQ4E",
-      "3ohzdOFQWcCZA8dRT2",
-      "l0MYMranKNhMUFveE",
-      "l378oRMuApI3a35Cg",
-      "3o7TKVA3gcjiKcFuV2",
-      "3oEduJXGwLsIe1RRPG",
-      "3og0IGjLXRttYbbtcc",
-      "n8k3O3KWXbPrPfsm7s",
-      "evlvEhB86RQidkiVBO",
-      "3oFzmiu86mdcjOsjOU",
-      "2bXyklhc7qQv0dTVXr",
-      "26xBs1E58r3ZHYvgQ",
-    ];
+      const smallSize = (
+        await sharp(wowifiedSmallSizePath, {
+          animated: true,
+        }).toBuffer()
+      ).toString("base64");
 
-    const gifId = gifs[Math.floor(Math.random() * gifs.length)];
-    const gifPath = `./libs/gifs/${gifId}.webp`;
+      console.log("Cleaning up");
+      fs.unlinkSync(inputPath);
+      fs.unlinkSync(removedBgImagePath);
+      fs.unlinkSync(wowifiedOriginalSizePath);
+      fs.unlinkSync(wowifiedSmallSizePath);
 
-    console.log("Wowifying");
+      console.log("Saving data to S3");
+      const s3 = new AWS.S3();
 
-    // Combine gif with result and save file to tmp directory
-    const wowifiedOriginalSizePath = `/tmp/${uuidv4()}.webp`;
+      await s3
+        .putObject({
+          Bucket: "image-wower-data",
+          Key: token,
+          Body: JSON.stringify({
+            wowifiedOriginal: originalSize,
+            wowifiedSmall: smallSize,
+          }),
+        })
+        .promise();
 
-    await sharp(gifPath, {
-      animated: true,
-    })
-      .composite([
-        {
-          input: removedBgImagePath,
-          tile: true,
-          top: 0,
-          left: 0,
-        },
-      ])
-      .webp()
-      .toFile(wowifiedOriginalSizePath);
+      console.log(`Deleting message: ${token}`);
+      const sqs = new AWS.SQS();
+      await sqs
+        .deleteMessage({
+          QueueUrl:
+            "https://sqs.us-west-2.amazonaws.com/368081326042/wow-emoji-queue",
+          ReceiptHandle: receiptHandle,
+        })
+        .promise();
+    }
+  }
 
-    const wowifiedSmallSizePath = `/tmp/${uuidv4()}.webp`;
-    await sharp(wowifiedOriginalSizePath, { animated: true })
-      .resize({ width: 64, height: 64 })
-      .webp({ lossless: true })
-      .toFile(wowifiedSmallSizePath);
+  // Otherwise, check for image data to submit to SQS and return token to poll with
+  else if (event.body) {
+    const sqs = new AWS.SQS();
+    const token = uuidv4();
+    try {
+      // Send message to SQS with content to be processed and token
+      await sqs
+        .sendMessage({
+          MessageBody: event.body,
+          MessageAttributes: { token: token },
+          QueueUrl:
+            "https://sqs.us-west-2.amazonaws.com/368081326042/wow-emoji-queue",
+        })
+        .promise();
 
-    // Generate original and small size data
-    const originalSize = (
-      await sharp(wowifiedOriginalSizePath, {
-        animated: true,
-      }).toBuffer()
-    ).toString("base64");
-
-    const smallSize = (
-      await sharp(wowifiedSmallSizePath, {
-        animated: true,
-      }).toBuffer()
-    ).toString("base64");
-
-    console.log("Cleaning up");
-    fs.unlinkSync(inputPath);
-    fs.unlinkSync(removedBgImagePath);
-    fs.unlinkSync(wowifiedOriginalSizePath);
-    fs.unlinkSync(wowifiedSmallSizePath);
-
-    console.log("Returning data");
-
-    return {
-      cookies: [],
-      isBase64Encoded: false,
-      statusCode: 200,
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify({
-        wowifiedOriginal: originalSize,
-        wowifiedSmall: smallSize,
-      }),
-    };
-  } catch (e) {
-    console.log(JSON.stringify(e));
-
-    return {
-      cookies: [],
-      isBase64Encoded: false,
-      statusCode: 400,
-      headers: {},
-      body: "",
-    };
+      return {
+        cookies: [],
+        isBase64Encoded: false,
+        statusCode: 202,
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ token: token }),
+      };
+    } catch (e) {
+      return {
+        cookies: [],
+        isBase64Encoded: false,
+        statusCode: 500,
+        headers: {},
+        body: "",
+      };
+    }
   }
 };
