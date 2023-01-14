96359532c60338b84327a9c89d19e0956dbe6590
Xavier Salazar <x.salazar1729@gmail.com>
2023-01-13 17:34:54 -0800

    Only allow SQS access from Lambda

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -2,3 +2,32 @@ resource "aws_sqs_queue" "instance" {
   name                       = "wow-emoji-queue"
   visibility_timeout_seconds = 90 // seconds
 }
+
+resource "aws_sqs_queue_policy" "instance" {
+  queue_url = aws_sqs_queue.instance.id
+
+  policy = <<POLICY
+    {
+      "Id": "Policy1673660011566",
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "Stmt1673660009311",
+          "Action": [
+            "sqs:DeleteMessage",
+            "sqs:SendMessage",
+            "sqs:GetQueueAttributes"
+          ],
+          "Effect": "Allow",
+          "Resource": "${aws_sqs_queue.instance.arn}",
+          "Condition": {
+            "ArnEquals": {
+              "aws:SourceArn": "${aws_lambda_function.instance.arn}"
+            }
+          },
+          "Principal": "*"
+        }
+      ]
+    }
+  POLICY
+}
