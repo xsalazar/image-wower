d8f9b5aa7292c62e8ed3c38e72a8b49b8bf56fab
Xavier Salazar <x.salazar1729@gmail.com>
2023-01-13 22:06:01 -0800

    Setup deadletter queue

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -1,7 +1,22 @@
 resource "aws_sqs_queue" "instance" {
   name                       = "wow-emoji-queue"
   visibility_timeout_seconds = 90 // seconds
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.instance_deadletter.arn
+    maxReceiveCount     = 5
+  })
 }
+
+resource "aws_sqs_queue" "instance_deadletter" {
+  name = "wow-emoji-queue-deadletter"
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.instance.arn]
+  })
+}
+
 
 resource "aws_sqs_queue_policy" "instance" {
   queue_url = aws_sqs_queue.instance.id
