2f250d336bb16e7ee0a50fc89e986f6052bc2455
Xavier Salazar <x.salazar1729@gmail.com>
2023-01-14 17:45:25 -0800

    Add explicit GET route integration to Lambda 🙊

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -10,7 +10,7 @@ resource "aws_apigatewayv2_api" "instance" {
   }
 }
 
-resource "aws_apigatewayv2_api_mapping" "example" {
+resource "aws_apigatewayv2_api_mapping" "instance" {
   api_id      = aws_apigatewayv2_api.instance.id
   domain_name = aws_apigatewayv2_domain_name.instance.id
   stage       = "$default"
@@ -37,9 +37,15 @@ resource "aws_apigatewayv2_integration" "instance" {
   payload_format_version = "2.0"
 }
 
-resource "aws_apigatewayv2_route" "instance" {
+resource "aws_apigatewayv2_route" "put_instance" {
   api_id    = aws_apigatewayv2_api.instance.id
   route_key = "PUT /"
+  target    = "integrations/${aws_apigatewayv2_integration.instance.id}"
+}
+
+resource "aws_apigatewayv2_route" "get_instance" {
+  api_id    = aws_apigatewayv2_api.instance.id
+  route_key = "GET /"
   target    = "integrations/${aws_apigatewayv2_integration.instance.id}"
 }
 
