023898f6d83b3745fe5c06828dede278910c367a
Xavier Salazar <x.salazar1729@gmail.com>
2024-12-24 15:44:08 -0800

    I guess you can't create both at the same time

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -3,25 +3,24 @@ resource "aws_sqs_queue" "rembg_input" {
   name                       = "wow-emoji-rembg-input-queue"
   visibility_timeout_seconds = 90 // seconds
 
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.rembg_input_deadletter.arn
-    maxReceiveCount     = 5
-  })
+  # redrive_policy = jsonencode({
+  #   deadLetterTargetArn = aws_sqs_queue.rembg_input_deadletter.arn
+  #   maxReceiveCount     = 5
+  # })
 }
 
-data "aws_sqs_queue" "rembg_input_data" {
-  name = aws_sqs_queue.rembg_input.name
-}
+# data "aws_sqs_queue" "rembg_input_data" {
+#   name = aws_sqs_queue.rembg_input.name
+# }
 
-resource "aws_sqs_queue" "rembg_input_deadletter" {
-  name = "wow-emoji-rembg-input-queue-deadletter"
+# resource "aws_sqs_queue" "rembg_input_deadletter" {
+#   name = "wow-emoji-rembg-input-queue-deadletter"
 
-  redrive_allow_policy = jsonencode({
-    redrivePermission = "byQueue",
-    sourceQueueArns   = [data.aws_sqs_queue.rembg_input_data.arn]
-  })
-}
-
+#   redrive_allow_policy = jsonencode({
+#     redrivePermission = "byQueue",
+#     sourceQueueArns   = [data.aws_sqs_queue.rembg_input_data.arn]
+#   })
+# }
 
 resource "aws_sqs_queue_policy" "rembg_policy" {
   queue_url = aws_sqs_queue.rembg_input.id
@@ -65,25 +64,24 @@ resource "aws_sqs_queue" "combiner_input" {
   name                       = "wow-emoji-combiner-input-queue"
   visibility_timeout_seconds = 90 // seconds
 
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.combiner_input_deadletter.arn
-    maxReceiveCount     = 5
-  })
+  # redrive_policy = jsonencode({
+  #   deadLetterTargetArn = aws_sqs_queue.combiner_input_deadletter.arn
+  #   maxReceiveCount     = 5
+  # })
 }
 
-data "aws_sqs_queue" "combiner_input_data" {
-  name = aws_sqs_queue.combiner_input.name
-}
+# data "aws_sqs_queue" "combiner_input_data" {
+#   name = aws_sqs_queue.combiner_input.name
+# }
 
-resource "aws_sqs_queue" "combiner_input_deadletter" {
-  name = "wow-emoji-combiner-input-queue-deadletter"
+# resource "aws_sqs_queue" "combiner_input_deadletter" {
+#   name = "wow-emoji-combiner-input-queue-deadletter"
 
-  redrive_allow_policy = jsonencode({
-    redrivePermission = "byQueue",
-    sourceQueueArns   = [data.aws_sqs_queue.combiner_input_data.arn]
-  })
-}
-
+#   redrive_allow_policy = jsonencode({
+#     redrivePermission = "byQueue",
+#     sourceQueueArns   = [data.aws_sqs_queue.combiner_input_data.arn]
+#   })
+# }
 
 resource "aws_sqs_queue_policy" "combiner_policy" {
   queue_url = aws_sqs_queue.combiner_input.id
