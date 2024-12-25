eac8f0029add56c1dd300ce1151503e2e2038437
Xavier Salazar <x.salazar1729@gmail.com>
2024-12-25 11:19:01 -0800

    Remove this

--- src/rembg/index.py
+++ src/rembg/index.py
@@ -25,7 +25,7 @@ def handler(event, context):
         print("Removing background")
 
         # Remove background
-        input = BytesIO(data["Body"].read())
+        input = data["Body"].read()
         output = remove(input)
 
         # Save to S3
