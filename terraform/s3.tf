00aad6380278adad26a2ba92e689a29660952c74
Xavier Salazar <x.salazar1729@gmail.com>
2025-01-04 11:23:15 -0800

    Add lifecycle rule to the data bucket to prune out old data

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,6 +2,17 @@ resource "aws_s3_bucket" "data" {
   bucket = "image-wower-data"
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "l1" {
+  bucket = aws_s3_bucket.data.id
+  rule {
+    status = "Enabled"
+    id     = "expire_all_files"
+    expiration {
+      days = 10
+    }
+  }
+}
+
 resource "aws_s3_bucket" "gifs" {
   bucket = "image-wower-gifs"
 }
